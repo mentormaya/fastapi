f623ed0b0fa33b8f0093e6926c1e9f3ff07227f3
Ajay Singh <ajay@mentormaya.com>
2022-06-14 10:02:37 +0545

    host updated

--- main.py
+++ main.py
@@ -55,4 +55,4 @@ def nepali_number(num):
     return res.nepali()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=int(os.getenv("PORT", default=3000)), log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=3000)), log_level="info", reload=True)
